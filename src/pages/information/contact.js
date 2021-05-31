d62513421e2262b7941ab8aa1ee73a044c5b41b5
Takeru Kayamori <takeru.kayamori@gmail.com>
2021-06-01 00:15:38 +0800

    trying material-UI

--- src/pages/information/contact.js
+++ src/pages/information/contact.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { TextField, Button } from '@material-ui/core';
 
 const ContactPage = () => {
     return(
         <Layout pageTitle="ContactPage">
+            <Button variant="contained" color="primary">Primary</Button>
             <p>This is contact</p>
+            {/* <form noValidate autoComplete="off">
+                <TextField id="standard-basic" label="Standard" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </form> */}
             <StaticImage src="../../images/woman1.jpg" />
         </Layout>
     )
